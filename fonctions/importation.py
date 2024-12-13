1f8171a1ecab13a086b8fe769170d5c941497e84
Najim Djareddir <najprgm@gmail.com>
2024-12-13 18:10:28 +0100

    Push de la fonction d'importation des données et test dans le notebook NAJIM

--- fonctions/importation.py
+++ fonctions/importation.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import glob
 import os
+import os.path
 
 def etl(path_folder_files : str) -> pd.DataFrame:
     
     full_data = pd.DataFrame()
     print("*"*50)
-    print("Démarrage du processus d'importation")
+    print("Démarrage du processus d'importation\n")
     
     for fichier in glob.glob(f"{path_folder_files}\*.csv"):
-        fichier = open(fichier, "r", encoding = "utf-8")
-        nom_fichier = os.path.basename(fichier).replace(".csv", "")
-        print(f"Importation du fichier {nom_fichier}")
-        
+        print(f"Importation du fichier {os.path.basename(fichier)}")
         df_temp = pd.read_csv(fichier, encoding='utf-8', compression='zip', sep = ',')
         
-        full_data = pd.concat([full_data, df_temp])
+        full_data = pd.concat([full_data, df_temp]).reset_index(drop = True)
     
     print("Processus d'importation terminé")
     print("*"*50)
+    print(f"\nAperçue des données :")
+    print(full_data)
     return full_data
