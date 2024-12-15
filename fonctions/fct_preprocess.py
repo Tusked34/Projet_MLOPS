5f4ac43d95cb7809cb21640fb6c7c123c7edbb61
Tusked34 <theo.lahmar-clavier.etu@univ-lille.fr>
2024-12-15 16:39:39 +0100

    nettoyage données aberrantes + passage de float a integer

--- fonctions/fct_preprocess.py
+++ fonctions/fct_preprocess.py
@@ -26,11 +26,12 @@ def assign_borough(df):
 
     # Apply the mapping function and update the DataFrame
     df["BOROUGH"] = df.apply(map_borough, axis=1)
-    return df
+    print("\n*** Grouping Borough OK ***")
 
 
 def drop_useless_columns(df):
     df = df.drop(['BROKERTITLE','STATE','LATITUDE', 'LONGITUDE','ADDRESS','LOCALITY', 'SUBLOCALITY', 'STREET_NAME', 'ADMINISTRATIVE_AREA_LEVEL_2', 'LONG_NAME','FORMATTED_ADDRESS','MAIN_ADDRESS'], axis=1, inplace=True)
+    print("\n*** Drop Useless Columns OK ***")
 
 
 def low_modalities_grouping(df):
@@ -42,3 +43,32 @@ def low_modalities_grouping(df):
     type_counts = df['TYPE'].value_counts()
     low_count_types = type_counts[type_counts < 50].index
     df['TYPE'] = df['TYPE'].replace(low_count_types, 'Autre_Type')
+    print("\n*** Low Modalities Grouping  OK ***")
+
+
+def filter_price_range(df, column='PRICE', min_price=49000, max_price=180000000):
+    """
+    Filtre directement le DataFrame passé en paramètre en supprimant les lignes où les valeurs
+    de la colonne spécifiée ne sont pas dans l'intervalle [min_price, max_price].
+
+    :param df: DataFrame à modifier.
+    :param column: Nom de la colonne contenant les valeurs à filtrer (par défaut 'PRICE').
+    :param min_price: Valeur minimale incluse (par défaut 49000).
+    :param max_price: Valeur maximale incluse (par défaut 180000000).
+    """
+    # Appliquer le filtre directement au DataFrame
+    df.drop(df[(df[column] < min_price) | (df[column] > max_price)].index, inplace=True)
+    print("\n*** Filter Price Range OK ***")
+
+def round_and_convert_to_int(df, columns):
+    """
+    Arrondit les valeurs des colonnes spécifiées à l'unité et les convertit en entier.
+
+    :param df: DataFrame à modifier.
+    :param columns: Liste des colonnes à traiter.
+    """
+    for column in columns:
+        # Arrondir les valeurs et les convertir en int
+        df[column] = df[column].round(0).astype(int)
+    
+    print("\n*** Round and Convert to Integer  OK ***")
