817a35eb5c7b2f8960f185fa2e5843f2ba121c1c
Tusked34 <theo.lahmar-clavier.etu@univ-lille.fr>
2024-12-15 15:27:58 +0100

    fin du preprocessing

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -5,18 +5,18 @@ import sys
 import os
 
 # Ajouter dynamiquement le chemin du dossier "fonctions" (revenir en arrière de deux niveaux)
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../fonctions')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from fonctions.importation import *
 from fonctions.fct_preprocess import *
 
 ### main :
-df = etl(r"..\data\data_raw")
+df = etl(r"data\data_raw")
 
 assign_borough(df)
 drop_useless_columns(df)
 low_modalities_grouping(df)
 print("\nData cleaning OK")
 
-df.to_csv('..\data\data_clean\data_preprocess.csv', index=False)
+df.to_csv(r'data\data_clean\data_preprocess.csv', index=False)
 print("\nClean data saved OK")
